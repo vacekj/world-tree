843c4dea31e53a0fc967096b9ff0dd9cb5570b66
Atris <vacekj@outlook.com>
2024-08-27 19:32:00 +0200

    updates

--- bin/tree_availability_service.rs
+++ bin/tree_availability_service.rs
@@ -42,8 +42,8 @@ pub async fn main() -> eyre::Result<()> {
         http_provider,
         config.provider.throttle.unwrap_or(u32::MAX),
         Some(Jitter::new(
-            Duration::from_millis(1_000),
-            Duration::from_millis(30_000),
+            Duration::from_millis(50),
+            Duration::from_millis(5_000),
         )),
     );
 
